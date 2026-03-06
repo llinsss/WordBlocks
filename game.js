2dc1dddd45ae8799894110a01527895b367d38fa
llins <llinsomoudu@gmail.com>
2026-03-06 11:33:00 +0100

    gnm

--- game.js
+++ game.js
@@ -1593,12 +1593,3 @@ window.endGame = function() {
     }
     originalEndGame();
 };
-
-// Parent Info Modal
-window.showParentInfo = function() {
-    document.getElementById('parent-info-modal').style.display = 'flex';
-};
-
-window.closeParentInfo = function() {
-    document.getElementById('parent-info-modal').style.display = 'none';
-};
